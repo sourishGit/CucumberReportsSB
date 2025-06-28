4b5bc776a0ade5d26a99fb2de00322d3e33a6950
sourish <sourish.vlogger@gmail.com>
2025-06-28 17:05:56 +0530

    Jenkins

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/cucumber", glue="sourishBakshiFrameworkAuto.StepDefinitions", 
 monochrome=true, tags="@Regression", plugin= {
-        "pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+        "pretty","json:target/cucumber-report.json" })
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 	
 	
